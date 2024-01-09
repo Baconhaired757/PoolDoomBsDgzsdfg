6d3397a0e0910c86fe1aaf8570527826956cbf0b
Baconhaired757 <80852510+Baconhaired757@users.noreply.github.com>
2024-01-08 18:52:05 -0800

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1929156.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 162848613, 162820181, 162839722, 162841441, 162841505, 162844478, 162844479, 162844480, 162838293, 162958864 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/162820184/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/162820185/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/162820183/1/ammo.js', 'preload' : true},
+];
